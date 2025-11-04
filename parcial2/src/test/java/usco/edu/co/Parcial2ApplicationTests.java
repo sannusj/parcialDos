8aa97c0ddaedf3e1d7ef359137345a9c929e49ec
santiago <dasanjaus2005@gmail.com>
2025-11-04 06:09:58 -0500

    initial commit parcial 2

---
+++ parcial2/src/test/java/usco/edu/co/Parcial2ApplicationTests.java
@@ -0,0 +1,13 @@
+package usco.edu.co;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Parcial2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
